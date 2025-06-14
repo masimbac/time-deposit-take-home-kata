4636bf5588ff7e01bac13f53a119dc7df1da4644
Masimba <masimba@paramasolutions.co.za>
2025-06-14 13:19:20 +0200

    Project structure: added springboot & jacoco

---
+++ java/src/test/java/org/ikigaidigital/TimeDepositKataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ikigaidigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDepositKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
